ad10c7574363b98c4a3b3056a3b2dcc6fec53a60
zouhuiying <1182535730@qq.com>
2015-08-10 10:50:03 +0800

    frist

---
+++ productListControllers.js
@@ -0,0 +1,40 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("sportsStore")
+    .constant("productListActiveClass", "btn-primary")
+    .constant("productListPageCount", 3)
+    .controller("productListCtrl", function ($scope, $filter,
+        productListActiveClass, productListPageCount) {
+
+        var selectedCategory = null;
+
+        $scope.selectedPage = 1;
+        $scope.pageSize = productListPageCount;
+
+        $scope.selectCategory = function (newCategory) {
+        console.log(newCategory+"--------");
+            selectedCategory = newCategory;
+            
+            $scope.selectedPage = 1;
+        }
+
+        $scope.selectPage = function (newPage) {
+            $scope.selectedPage = newPage;
+        }
+
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory == null ||
+                product.category == selectedCategory;
+        }
+
+        $scope.getCategoryClass = function (category) {
+        	console.log(category+"--asdadsasd");
+            return selectedCategory == category ? productListActiveClass : "";
+            //return productListActiveClass;
+        }
+
+        $scope.getPageClass = function (page) {
+            return $scope.selectedPage == page ? productListActiveClass : "";
+        }
+    });
+
